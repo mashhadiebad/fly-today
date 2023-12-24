ca08c88370352a103638db2569e4829e3dea3ed4
mh_mashhadiebad <mh.mashhadiebad@gmail.com>
2023-12-24 10:28:35 +0330

    Fix sort bugs

--- app/components/Dropdown/Dropdown.tsx
+++ app/components/Dropdown/Dropdown.tsx
@@ -1,9 +1,62 @@
+"use client";
+import { useEffect, useState } from "react";
 import style from "./Dropdown.module.scss";
+import { IoIosArrowDown } from "react-icons/io";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
 
-const Dropdown = () => {
+interface Props {
+  content: { title: string; value: string }[];
+  sendValue: Function;
+  defaultValue: string;
+}
+
+const Dropdown = ({ content, sendValue, defaultValue }: Props) => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+  const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
+  const [selectedTitle, setSelectedTitle] = useState<string>(content[0].title);
+  const handleCheckboxChange = (valueItem: string, titleItem: string): void => {
+    setSelectedValue(valueItem);
+    setSelectedTitle(titleItem);
+  };
+
+  useEffect(() => {
+    sendValue(selectedValue);
+    setTimeout(() => {
+      setIsDropdownVisible(false);
+    }, 200);
+  }, [selectedValue]);
   return (
-    <div>
-      <div className={style.dropdown__container}></div>
+    <div className={style.dropdown__container}>
+      <div
+        className={style.dropdown__header}
+        onClick={() => setIsDropdownVisible(!isDropdownVisible)}
+      >
+        <div className={style.dropdown__title}>{selectedTitle}</div>
+        <IoIosArrowDown
+          style={{
+            transform: isDropdownVisible ? "rotate(180deg)" : "none",
+            color: "#999999",
+          }}
+        />
+      </div>
+      {isDropdownVisible && (
+        <div className={style.dropdown__content}>
+          {content.map((item) => (
+            <div
+              key={item.value}
+              className={style.dropdown__item}
+              onClick={() => handleCheckboxChange(item.value, item.title)}
+            >
+              <input
+                type="checkbox"
+                checked={selectedValue === item.value}
+                className="checkbox"
+              />
+              <div className={style.dropdown__itemTitle}>{item.title}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- app/components/Sort/Sort.tsx
+++ app/components/Sort/Sort.tsx
@@ -1,27 +1,40 @@
 "use client";
 import style from "./Sort.module.scss";
-import { ChangeEvent, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { CgSortAz } from "react-icons/cg";
 import { useDispatch } from "react-redux";
 import { sortActions } from "@/store/store";
 import { useAppSelector } from "@/store/store";
+import Dropdown from "../Dropdown/Dropdown";
 
 const Sort = () => {
   const dispatch = useDispatch();
-  const sortTypeName = useAppSelector((state) => state.sort.sortType);
+  const sortTypeName: string = useAppSelector((state) => state.sort.sortType);
   const [isSortVisible, setIsSortVisible] = useState<boolean>(false);
   const [sortType, setSortType] = useState<string>(sortTypeName);
 
-  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setSortType(event.target.value);
-    dispatch(sortActions.setSortType(event.target.value));
+  const desktopDropdownContent = [
+    { title: "قیمت", value: "totalFare" },
+    { title: "زمان پرواز", value: "journeyDuration" },
+  ];
+
+  const handleSelectChange = (value: string) => {
+    setSortType(value);
   };
+  const handleMobileSort = () => {
+    setSortType(sortTypeName);
+    setIsSortVisible(!isSortVisible);
+  };
+
+  useEffect(() => {
+    dispatch(sortActions.setSortType(sortType));
+  }, [sortType]);
 
   return (
     <div className="relative">
       <div
         className={style.mobileDesign__sortButton}
-        onClick={() => setIsSortVisible(!isSortVisible)}
+        onClick={handleMobileSort}
       >
         <CgSortAz style={{ marginLeft: "8px", fontSize: "20px" }} />
         <p>مرتب سازی</p>
@@ -29,44 +42,50 @@ const Sort = () => {
       {isSortVisible && (
         <div className={style.mobileDesign__sortBox}>
           <div className={style.mobileDesign__sortContent}>
-            <div
-              className="form-control cursor-pointer"
-              onClick={() => {
-                setSortType("totalFare");
-                dispatch(sortActions.setSortType("totalFare"));
-              }}
-            >
-              <label className="label cursor-pointer">
+            <div className="form-control cursor-pointer">
+              <label
+                className="label cursor-pointer justify-start"
+                onClick={() => {
+                  setSortType("totalFare");
+                  setTimeout(() => {
+                    setIsSortVisible(!isSortVisible);
+                  }, 200);
+                }}
+              >
+                <input
+                  value="totalFare"
+                  type="checkbox"
+                  className="checkbox checkbox-info w-4 h-4"
+                  checked={sortType === "totalFare"}
+                />
                 <span
-                  className={`label-text ${style.mobileDesign__sortContent}`}
+                  className={`label-text ${style.mobileDesign__sortContent} mr-2`}
                 >
                   قیمت
                 </span>
-                <input
-                  value="totalFare"
-                  type="radio"
-                  name="radio-10"
-                  className="radio checked:bg-blue-500"
-                  checked={sortType === "totalFare"}
-                />
               </label>
             </div>
-            <div
-              className="form-control cursor-pointer"
-              onClick={() => {
-                setSortType("journeyDuration");
-                dispatch(sortActions.setSortType("journeyDuration"));
-              }}
-            >
-              <label className="label cursor-pointer">
-                <span className="label-text text-right">زمان پرواز</span>
+            <div className="form-control cursor-pointer">
+              <label
+                className="label cursor-pointer justify-start"
+                onClick={() => {
+                  setSortType("journeyDuration");
+                  setTimeout(() => {
+                    setIsSortVisible(!isSortVisible);
+                  }, 200);
+                }}
+              >
                 <input
                   value="journeyDuration"
-                  type="radio"
-                  name="radio-10"
-                  className="radio checked:bg-blue-500"
+                  type="checkbox"
+                  className="checkbox checkbox-info w-4 h-4"
                   checked={sortType === "journeyDuration"}
                 />
+                <span
+                  className={`label-text ${style.mobileDesign__sortContent} mr-2`}
+                >
+                  زمان پرواز
+                </span>
               </label>
             </div>
           </div>
@@ -74,20 +93,11 @@ const Sort = () => {
       )}
       <div className={style.desktopDesign__sortContainer}>
         <div className={style.desktopDesign__sortTitle}>مرتب سازی:</div>
-        <select
-          className={`select select-bordered w-full max-w-xs ${style.desktopDesign__sortDropdown}`}
-          onChange={handleSelectChange}
-        >
-          <option value="totalFare" selected={sortType === "totalFare"}>
-            قیمت
-          </option>
-          <option
-            value="journeyDuration"
-            selected={sortType === "journeyDuration"}
-          >
-            زمان پرواز
-          </option>
-        </select>
+        <Dropdown
+          sendValue={handleSelectChange}
+          content={desktopDropdownContent}
+          defaultValue={sortTypeName}
+        />
       </div>
     </div>
   );
